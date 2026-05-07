fc00d1ac07ed94804ec44a7c3cc3f476511eefe2
Daniel Kerr <danielkerr@users.noreply.github.com>
2026-05-08 01:26:57 +0800

    Update controller.js

--- upload/assets/framework/library/controller.js
+++ upload/assets/framework/library/controller.js
@@ -1,3 +1,11 @@
 export class Controller {
+    constructor(element) {
+        let methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
 
+        for (let method of methods) {
+            if (element[method] == undefined && typeof this[method] === 'function') {
+                element[method] = this[method].bind(this);
+            }
+        }
+    }
 }
